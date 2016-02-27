ee9cc15b8f50117d3cae66e94ab0052ee89f58a7
Runli <ilnur.gazizov@phystech.edu>
2016-02-27 03:33:44 +0300

    add NumbSearchApp

---
+++ NumbSearcherApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace NumbSearcherApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private WorkClass worker = null;
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e) {
+            int countOfThreads = 0;
+            int targetNumber = 0;
+            int interval = 0;
+            worker = WorkClass.GetWorkClassInstance0();
+            switch (btnStart.Content as string) {
+                case "Start":       
+                    btnStart.Content = "Stop";
+                    lbxResults.Items.Clear();
+                    try {
+                        countOfThreads = Int32.Parse(txbCountOfThreads.Text);
+                        targetNumber = Int32.Parse(txbTargetNumber.Text);
+                        interval = Int32.Parse(txbInterval.Text);
+                    } catch (FormatException ex) {
+                        MessageBox.Show(ex.Message);
+                    }
+                    if (countOfThreads > 0 && -1 < targetNumber && targetNumber < 100 && -1 < interval) {
+                        worker.CreateAndStartSearch(countOfThreads, targetNumber, interval);
+                    } else MessageBox.Show("Error. Число потоков должно быть положительным. Интервал должен быть неотрицательным. Искомое число должно быть в диапазоне 0 - 99");
+                    break;
+                case "Stop":
+                    // тут вызов метода для остановки работы
+                    if(!worker.Equals(null))
+                        worker.StopAll();
+                    btnStart.Content = "Start";
+                    break;
+                default:
+                    break;
+            }            
+
+        }
+    }
+}
---
+++ NumbSearcherApp/WorkClass.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NumbSearcherApp {
+    class WorkClass {
+        private static WorkClass instance;
+        private static object syncRoot = new Object();
+
+        private Random random = new Random();
+        private DateTime localDate;
+
+        private List<Thread> threads;
+        private int countOfThreads;
+        private int targetNumber;
+        private int interval;
+        bool b;
+        ListBox lb = Application.Current.MainWindow.FindName("lbxResults") as ListBox;
+
+        private WorkClass() { }
+        public static WorkClass GetWorkClassInstance0() {
+            if (instance == null) {
+                lock (syncRoot) {
+                    if (instance == null) instance = new WorkClass();
+                }
+            }
+            return instance;
+        }
+        public void CreateAndStartSearch(int _countOfThreads, int _targetNumber, int _interval) {
+            countOfThreads = _countOfThreads;
+            targetNumber = _targetNumber;
+            interval = _interval;
+            threads = new List<Thread>(countOfThreads);
+            b = true;
+            for (int i = 0; i < countOfThreads; i++) {
+                threads.Add(new Thread(new ThreadStart(DoSearch)));
+                threads[i].Name = string.Format("Thread #{0}", i);
+                threads[i].IsBackground = true;
+            }
+            foreach (Thread t in threads) {
+                if (!t.Equals(null)) {
+                    t.Start();
+                }
+            }
+        }
+        // main method to search targetNumber
+        private void DoSearch() {
+            int currentNumber = -1;
+            Action<bool, string> a = (i, s) => {
+                ListBoxItem item = new ListBoxItem();
+                item.IsSelected = true;
+                item.Content = s;
+                if (i) item.Background = new LinearGradientBrush(Colors.ForestGreen, Colors.LightGreen, 90);
+                else item.Background = new LinearGradientBrush(Colors.OrangeRed, Colors.Orange, 90);
+                item.FontFamily = new FontFamily("Tahoma");
+                lb.Items.Add(item);
+            };
+
+            while (b) {
+                lock (syncRoot) {
+                    if (b == false) {
+                        break;
+                    }
+                    currentNumber = random.Next(100);
+                    localDate = DateTime.Now;
+                    string threadInfo = string.Format("{0}\n" + Thread.CurrentThread.Name +
+                        " - " + localDate.ToString(new CultureInfo("ru-RU")), currentNumber);
+
+                    //Console.WriteLine(threadInfo);    // write to console
+                    if (currentNumber != targetNumber) {
+                        if (currentNumber < targetNumber) {
+                            lb.Dispatcher.Invoke(a, true, threadInfo);
+                        } else lb.Dispatcher.Invoke(a, false, threadInfo);
+                        Thread.Sleep(interval);
+                    } else if (currentNumber == targetNumber) {
+                        lb.Dispatcher.Invoke(a, true, threadInfo);
+                        MessageBox.Show("I found your number!\n" + threadInfo);
+                        //Console.WriteLine("I found your number!");
+                        b = false;
+                        StopAll();  // stop all threads
+                    }
+                }
+            }
+            Console.WriteLine("end of function"); 
+        }
+
+        internal void StopAll() {
+            foreach (Thread t in threads) {
+                t.Abort();
+            }
+            threads = null;
+        }
+    }
+}
+
